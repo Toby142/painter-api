acf92e40b94112f885cb4ddbfce834340f43afc5
Toby142 <tobiasvandeven142@gmail.com>
2023-04-11 16:23:25 +0200

    project start

---
+++ src/main/java/com/example/painterapi/PainterApiApplication.java
@@ -0,0 +1,24 @@
+package com.example.painterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+ import org.springframework.web.bind.annotation.GetMapping;
+ import org.springframework.web.bind.annotation.RestController;
+
+
+
+@SpringBootApplication
+@RestController
+public class PainterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PainterApiApplication.class, args);
+	}
+
+	@GetMapping("/ping")
+	public String ping() {
+		return "pong";
+	}
+
+}
