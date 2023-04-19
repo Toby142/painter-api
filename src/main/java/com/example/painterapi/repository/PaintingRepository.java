9638511fd448176cc6324bff0e4a6ecb5d7b2069
Toby142 <tobiasvandeven142@gmail.com>
2023-04-19 14:45:29 +0200

    painting added

--- src/main/java/com/example/painterapi/repository/PaintingRepository.java
+++ src/main/java/com/example/painterapi/repository/PaintingRepository.java
@@ -1,10 +1,2 @@
-//package com.example.painterapi.repository;
-//
-//import org.springframework.data.repository.CrudRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import com.example.painterapi.models.Painting;
-//
-//@Repository
-//public interface PaintingRepository extends CrudRepository<Painting, Integer> {
-//}
+package com.example.painterapi.repository;public interface PaintingRepository {
+}
